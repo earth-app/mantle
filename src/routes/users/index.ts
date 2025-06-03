1dd3a535d53d7559bbc66433acebd3e4efdc699d
Gregory Mitchell <54124162+gmitch215@users.noreply.github.com>
2025-06-03 00:36:34 -0500

    feat: paginated users route

--- src/routes/users/index.ts
+++ src/routes/users/index.ts
@@ -1,22 +1,73 @@
 import { Hono } from "hono"
 
+import { describeRoute } from "hono-openapi"
+import { resolver } from "hono-openapi/zod"
+import * as schemas from "../../openapi/schemas"
+import * as tags from "../../openapi/tags"
+
+
 // User Routes
 import loginUser from './login'
 import createUser from './create'
 import user from './user'
 import { bearerAuthMiddleware } from "../../util/authentication"
+import { getUsers } from "../../util/users"
+import Bindings from "../../bindings"
 
 // Implementation
 
-const routes = new Hono()
+const users = new Hono<{ Bindings: Bindings }>()
 
-routes.route('/login', loginUser)
+users.get(
+    '/',
+    describeRoute({
+        summary: "Retrieve a paginated list of all users",
+        description: "Gets a paginated list of all users in the Earth App.",
+        security: [{ BasicAuth: [] }],
+        responses: {
+            200: {
+                description: "List of users",
+                content: {
+                    'application/json': {
+                        schema: resolver(schemas.users),
+                    }
+                }
+            },
+            400: schemas.badRequest
+        },
+        tags: [tags.USERS]
+    }),
+    async (c) => {
+        const page = c.req.query('page') ? parseInt(c.req.query('page')!) : 1
+        const limit = c.req.query('limit') ? parseInt(c.req.query('limit')!) : 25
 
-routes.route('/create', createUser)
+        if (isNaN(page) || isNaN(limit) || page < 1 || limit < 1) {
+            return c.json({
+                code: 400,
+                message: "Invalid pagination parameters"
+            }, 400)
+        }
+        
 
-routes.route('/current', user)
-routes.use('/current', bearerAuthMiddleware())
+        if (limit > 100) {
+            return c.json({
+                code: 400,
+                message: "Limit cannot exceed 100"
+            }, 400)
+        }
 
-routes.route('/:id', user)
+        const users = (await getUsers(c.env, limit, page - 1)).map(user => user.public)
+        return c.json(users, 200)
+    }
+)
 
-export default routes
+users.route('/login', loginUser)
+
+users.route('/create', createUser)
+
+users.route('/current', user)
+users.use('/current', bearerAuthMiddleware())
+
+users.route('/:id', user)
+
+export default users
