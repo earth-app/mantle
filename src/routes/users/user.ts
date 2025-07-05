faee4e4e27f7abdb4d305791ac2b9176acbd2680
Gregory Mitchell <54124162+gmitch215@users.noreply.github.com>
2025-07-05 01:36:55 -0500

    feat: reorganize and optimize user routes

--- src/routes/users/user.ts
+++ src/routes/users/user.ts
@@ -1,10 +1,17 @@
-import { Hono } from 'hono';
+import { Context, Hono } from 'hono';
+
+import { describeRoute } from 'hono-openapi';
+import type { OpenAPIV3 } from 'openapi-types';
+import zodToJsonSchema from 'zod-to-json-schema';
+import * as schemas from '../../openapi/schemas';
+import * as tags from '../../openapi/tags';
 
 import Bindings from '../../bindings';
-import { deleteUser, getUserById, getUserByUsername, patchUser } from '../../util/routes/users';
+import { deleteUser, getUserById, getUserByUsername, getUserFromContext, patchUser } from '../../util/routes/users';
 import { bearerAuthMiddleware, getOwnerOfToken } from '../../util/authentication';
-import { UserObject } from '../../types/users';
+import { User, UserObject } from '../../types/users';
 import { com } from '@earth-app/ocean';
+import { ContentfulStatusCode } from 'hono/utils/http-status';
 
 const user = new Hono<{ Bindings: Bindings }>();
 
@@ -27,7 +34,6 @@ user.get('/', async (c) => {
 		}
 
 		const token = bearerToken.slice(7);
-
 		user = await getOwnerOfToken(token, c.env);
 		if (!user) {
 			return c.json(
@@ -127,63 +133,18 @@ user.patch('/', bearerAuthMiddleware(), async (c) => {
 		);
 	}
 
-	const bearerToken = c.req.header('Authorization');
-	if (!bearerToken || !bearerToken.startsWith('Bearer ')) {
+	const res = await getUserFromContext(c);
+	if (!res.data) {
 		return c.json(
 			{
-				code: 401,
-				message: 'Unauthorized'
+				code: res.status,
+				message: res.message
 			},
-			401
+			res.status as ContentfulStatusCode
 		);
 	}
 
-	const token = bearerToken.slice(7);
-	const path = c.req.param('id');
-	let user: UserObject | null;
-
-	// Current User
-	if (!path) {
-		user = await getOwnerOfToken(token, c.env);
-		if (!user) {
-			return c.json(
-				{
-					code: 401,
-					message: 'Unauthorized'
-				},
-				401
-			);
-		}
-	} else {
-		if (token !== c.env.ADMIN_API_KEY) {
-			return c.json(
-				{
-					code: 403,
-					message: 'Forbidden: You do not have permission to update this user.'
-				},
-				403
-			);
-		}
-
-		if (path.startsWith('@')) {
-			// By Username
-			const username = path.slice(1);
-			user = await getUserByUsername(username, c.env);
-		} else {
-			// By ID
-			user = await getUserById(path, c.env);
-		}
-	}
-
-	if (!user) {
-		return c.json(
-			{
-				code: 404,
-				message: 'User not found'
-			},
-			404
-		);
-	}
+	const user = res.data;
 
 	if (data.type || data.id) {
 		data = {
@@ -202,68 +163,125 @@ user.patch('/', bearerAuthMiddleware(), async (c) => {
 
 // Delete User
 user.delete('/', bearerAuthMiddleware(), async (c) => {
-	const bearerToken = c.req.header('Authorization');
-	if (!bearerToken || !bearerToken.startsWith('Bearer ')) {
+	const res = await getUserFromContext(c);
+	if (!res.data) {
 		return c.json(
 			{
-				code: 401,
-				message: 'Unauthorized'
+				code: res.status,
+				message: res.message
 			},
-			401
+			res.status as ContentfulStatusCode
 		);
 	}
-
-	const token = bearerToken.slice(7);
-	const path = c.req.param('id');
-	let user: UserObject | null;
-
-	// Current User
-	if (!path) {
-		user = await getOwnerOfToken(token, c.env);
-		if (!user) {
-			return c.json(
-				{
-					code: 401,
-					message: 'Unauthorized'
-				},
-				401
-			);
-		}
-	} else {
-		if (token !== c.env.ADMIN_API_KEY) {
-			return c.json(
-				{
-					code: 403,
-					message: 'Forbidden: You do not have permission to delete this user.'
-				},
-				403
-			);
-		}
-
-		if (path.startsWith('@')) {
-			// By Username
-			const username = path.slice(1);
-			user = await getUserByUsername(username, c.env);
-		} else {
-			// By ID
-			user = await getUserById(path, c.env);
-		}
-	}
-
-	if (!user) {
-		return c.json(
-			{
-				code: 404,
-				message: 'User not found'
-			},
-			404
-		);
-	}
-
-	// Delete the user
+	const user = res.data;
 	await deleteUser(user.account.id, c.env);
 
 	return c.body(null, 204);
 });
+
+// Change Field Privacy
+user.patch(
+	'/field_privacy',
+	describeRoute({
+		summary: 'Change Field Privacy',
+		description: 'Change the visibility of account fields in the user account.',
+		security: [{ BearerAuth: [] }],
+		requestBody: {
+			description: 'Field privacy object',
+			required: true,
+			content: {
+				'application/json': {
+					schema: zodToJsonSchema(schemas.userFieldPrivacy) as OpenAPIV3.SchemaObject
+				}
+			}
+		},
+		responses: {
+			200: {
+				description: 'User visibility updated successfully',
+				content: {
+					'application/json': {
+						schema: zodToJsonSchema(schemas.user) as OpenAPIV3.SchemaObject
+					}
+				}
+			},
+			400: schemas.badRequest
+		},
+		tags: [tags.USERS]
+	}),
+	bearerAuthMiddleware(),
+	async (c) => {
+		const rawBody = await c.req.text();
+		if (!rawBody) {
+			return c.json(
+				{
+					code: 400,
+					message: 'Request body cannot be empty'
+				},
+				400
+			);
+		}
+
+		let data: Partial<User['account']['visibility']> = await c.req.json();
+		if (!data || typeof data !== 'object') {
+			return c.json(
+				{
+					code: 400,
+					message: 'Invalid request body'
+				},
+				400
+			);
+		}
+
+		const res = await getUserFromContext(c);
+		if (!res.data) {
+			return c.json(
+				{
+					code: res.status,
+					message: res.message
+				},
+				res.status as ContentfulStatusCode
+			);
+		}
+
+		const user = res.data;
+
+		// Sanitize Body
+		const allowedFields = Object.keys(user.public.account.visibility);
+
+		// Filter data to only include allowed fields
+		const sanitizedData: Partial<User['account']['visibility']> = Object.keys(data)
+			.filter((key) => allowedFields.includes(key))
+			.reduce((obj, key) => {
+				obj[key] = (data as any)[key];
+				return obj;
+			}, {} as any);
+
+		for (const [key, value] of Object.entries(sanitizedData)) {
+			if (key === 'account') continue;
+			if (key in user.account.visibility) {
+				user.account.setFieldPrivacy(key, com.earthapp.account.Privacy.valueOf(value as string));
+			} else {
+				// In case sanitization failed
+				console.warn(`Invalid field: ${key} in user visibility update for ${user.public.username}`);
+				return c.json(
+					{
+						code: 400,
+						message: `Invalid field: ${key}`
+					},
+					400
+				);
+			}
+		}
+
+		await patchUser(
+			user.account,
+			{ visibility: com.earthapp.Visibility.valueOf(sanitizedData.account ?? user.public.account.visibility.account) },
+			c.env
+		);
+		const updated = (await getUserById(user.account.id, c.env))!.public;
+
+		return c.json(updated, 200);
+	}
+);
 
 export default user;
--- src/types/users.ts
+++ src/types/users.ts
@@ -1,6 +1,7 @@
-import * as ocean from '@earth-app/ocean';
+import { com } from '@earth-app/ocean';
 import { DBUser } from '../util/routes/users';
 import { DBEvent } from '../util/routes/events';
+import { Event } from './events';
 
 // Users
 
@@ -19,9 +20,9 @@ export type UserObject = {
 	database: DBUser;
 	/**
 	 * The account object associated with the user.
-	 * This is an instance of ocean.com.earthapp.account.Account.
+	 * This is an instance of com.earthapp.account.Account.
 	 */
-	account: ocean.com.earthapp.account.Account;
+	account: com.earthapp.account.Account;
 };
 
 /**
@@ -41,43 +42,115 @@ export type User = {
 		firstName?: string;
 		lastName?: string;
 		username: string;
+		bio?: string;
 		email?: string;
-		address?: string | null;
+		address?: string;
 		country?: string;
 		phoneNumber?: number;
-		activities?: {
-			id: string;
-			name: string;
-			type: typeof ocean.com.earthapp.activity.ActivityType.prototype.name;
-		}[];
+		visibility: {
+			account: typeof com.earthapp.Visibility.prototype.name;
+			name: typeof com.earthapp.account.Privacy.prototype.name;
+			bio: typeof com.earthapp.account.Privacy.prototype.name;
+			email: typeof com.earthapp.account.Privacy.prototype.name;
+			address: typeof com.earthapp.account.Privacy.prototype.name;
+			activities: typeof com.earthapp.account.Privacy.prototype.name;
+			events: typeof com.earthapp.account.Privacy.prototype.name;
+			friends: typeof com.earthapp.account.Privacy.prototype.name;
+			last_login: typeof com.earthapp.account.Privacy.prototype.name;
+			account_type: typeof com.earthapp.account.Privacy.prototype.name;
+		};
 	};
+	activities?: {
+		id: string;
+		name: string;
+		types: (typeof com.earthapp.activity.ActivityType.prototype.name)[];
+	}[];
 };
 
 /**
  * Converts an Account object to a User object.
  * @param data The account data to convert.
+ * @param privacy The privacy accessibility for the respective fields.
  * @param created_at The creation date of the user (default is current date).
  * @param updated_at The last updated date of the user (default is current date).
  * @param last_login The last login date of the user (default is current date).
  * @returns A User object.
  */
 export function toUser(
-	data: ocean.com.earthapp.account.Account,
+	data: com.earthapp.account.Account,
+	privacy: com.earthapp.account.Privacy,
 	created_at: Date = new Date(),
 	updated_at: Date = new Date(),
 	last_login: Date = new Date()
 ): User {
-	const fullName = data.firstName && data.lastName ? `${data.firstName} ${data.lastName}` : undefined;
+	const fullName =
+		!data.isFieldPrivate('name', privacy) && data.firstName && data.lastName ? `${data.firstName} ${data.lastName}` : undefined;
 
-	return {
+	const user: User = {
 		id: data.id,
 		username: data.username,
 		fullName: fullName,
 		created_at: created_at,
 		updated_at: updated_at,
 		last_login: last_login,
-		account: JSON.parse(data.toJson())
+		account: {
+			...JSON.parse(data.toJson()),
+			visibility: {
+				account: data.visibility.name,
+				name: data.getFieldPrivacy('name').name,
+				bio: data.getFieldPrivacy('bio').name,
+				email: data.getFieldPrivacy('email').name,
+				address: data.getFieldPrivacy('address').name,
+				activities: data.getFieldPrivacy('activities').name,
+				events: data.getFieldPrivacy('events').name,
+				friends: data.getFieldPrivacy('friends').name,
+				last_login: data.getFieldPrivacy('last_login').name,
+				account_type: data.getFieldPrivacy('account_type').name
+			}
+		}
 	};
+
+	// First and Last Name
+	if (data.isFieldPrivate('name', privacy)) {
+		user.account.firstName = undefined;
+		user.account.lastName = undefined;
+	}
+
+	// Bio
+	if (data.isFieldPrivate('bio', privacy)) {
+		user.account.bio = undefined;
+	}
+
+	// Email
+	if (data.isFieldPrivate('email', privacy)) {
+		user.account.email = undefined;
+	}
+
+	// Address
+	if (data.isFieldPrivate('address', privacy)) {
+		user.account.address = undefined;
+	}
+
+	// Country
+	if (data.isFieldPrivate('country', privacy)) {
+		user.account.country = undefined;
+	}
+
+	// Phone Number
+	if (data.isFieldPrivate('phoneNumber', privacy)) {
+		user.account.phoneNumber = undefined;
+	}
+
+	// Activities
+	if (!data.isFieldPrivate('activities', privacy)) {
+		user.activities = data.activities.asJsArrayView().map((activity) => ({
+			id: activity.id,
+			name: activity.name,
+			types: activity.types.asJsArrayView().map((type) => type.name)
+		}));
+	}
+
+	return user;
 }
 
 /**
