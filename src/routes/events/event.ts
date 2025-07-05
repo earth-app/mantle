05af64972f8f9ba526e8cd37f18c03cf4d777cbf
Gregory Mitchell <54124162+gmitch215@users.noreply.github.com>
2025-07-05 13:04:09 -0500

    feat(draft): update event

--- src/routes/events/event.ts
+++ src/routes/events/event.ts
@@ -9,6 +9,7 @@ import * as tags from '../../openapi/tags';
 
 import Bindings from '../../bindings';
 import { getEventById } from '../../util/routes/events';
+import { Event } from '../../types/events';
 
 const event = new Hono<{ Bindings: Bindings }>();
 
@@ -70,6 +71,99 @@ event.get(
 		}
 
 		return c.json(event.public, 200);
+	}
+);
+
+// Update Event
+event.patch(
+	'/',
+	describeRoute({
+		summary: 'Update an event',
+		description: 'Updates an existing event by its ID.',
+		parameters: [
+			{
+				name: 'eventId',
+				in: 'path',
+				description: 'Event ID',
+				required: true,
+				schema: {
+					type: 'string',
+					maxLength: 100
+				}
+			}
+		],
+		requestBody: {
+			description: 'Event data to update',
+			required: true,
+			content: {
+				'application/json': {
+					schema: zodToJsonSchema(schemas.eventUpdate) as OpenAPIV3.SchemaObject
+				}
+			}
+		},
+		responses: {
+			200: {
+				description: 'Event updated successfully',
+				content: {
+					'application/json': {
+						schema: resolver(schemas.event)
+					}
+				}
+			},
+			400: schemas.badRequest,
+			404: {
+				description: 'Event not found',
+				content: {
+					'application/json': {
+						schema: resolver(schemas.error(404, 'Event not found'))
+					}
+				}
+			}
+		},
+		tags: [tags.EVENTS]
+	}),
+	async (c) => {
+		const id = c.req.param('eventId');
+		if (!id) {
+			return c.json(
+				{
+					code: 400,
+					message: 'Event ID is required'
+				},
+				400
+			);
+		}
+
+		const data = await c.req.json<Partial<Event>>();
+		if (!data || typeof data !== 'object') {
+			return c.json(
+				{
+					code: 400,
+					message: 'Invalid request body'
+				},
+				400
+			);
+		}
+
+		if (data.id || data.hostId) {
+			data.id = undefined; // Prevent updating ID
+			data.hostId = undefined; // Prevent updating host ID
+		}
+
+		const updatedEvent = await getEventById(id, c.env.DB);
+		if (!updatedEvent) {
+			return c.json(
+				{
+					code: 404,
+					message: 'Event not found'
+				},
+				404
+			);
+		}
+
+		// TODO Implement actual update logic
+
+		return c.json(updatedEvent.public, 200);
 	}
 );
 
