36f099fd771429250c2484a4d11dc6eb23b48859
Gregory Mitchell <54124162+gmitch215@users.noreply.github.com>
2025-07-17 23:21:41 -0500

    chore: add KV namespace, remove unused development var

--- src/bindings.ts
+++ src/bindings.ts
@@ -2,14 +2,13 @@ import { D1Database, KVNamespace } from '@cloudflare/workers-types';
 
 type Bindings = {
 	DB: D1Database;
-	earth_app: KVNamespace;
+	KV: KVNamespace;
 	ANONYMOUS_RATE_LIMIT: any;
 	AUTH_RATE_LIMIT: any;
 
 	// Secrets
 	KEK: string;
 	LOOKUP_HMAC_KEY: string;
-	DEVEOPMENT_TOKEN: string;
 	ADMIN_API_KEY: string;
 };
 
